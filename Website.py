0a17c33f0cc55d490af7d31a8a84865af12e3c08
unknown <ddinhminhhair@gmail.com>
2024-09-13 15:43:13 +0700

    Update Website.py

--- Website.py
+++ Website.py
@@ -17,18 +17,15 @@ def main():
     # Input box for the user to type a mathematical problem
     problem = st.text_area("Enter a mathematical problem:")
 
-
-    # if problem:
-    #     # Solve the problem
-    #     solution = solve_math_problem(problem)
-        
-    #     # Display the result with MathJax rendering
-    #     st.markdown(f"**Solution:**")
-    #     st.markdown(solution)
-    
     if st.button("Giải bài"):
         if problem:
-            solution = f"Lời giải cho bài toán '{problem}' sẽ được hiển thị ở đây."
+
+            # Solve the problem
+            st.write("Lời giải\n")
+            solution = solve_math_problem(problem)
+
+            # Display the result with MathJax rendering
+            st.markdown(solution)    
         else:
             st.write("Vui lòng nhập đề bài trước khi bấm 'Giải bài.'")    
 
